fc6d93251def340303893a8df260d35ecde65383
Sarah K <sarah.j.kerr10@gmail.com>
2019-08-21 09:42:49 -0400

    fixed typo

--- food_query.py
+++ food_query.py
@@ -4,7 +4,7 @@ import json
 
 class Food:
     
-    def __init__(self, api_key, keword, limit):
+    def __init__(self, api_key, keyword, limit):
         self.key = api_key
         self.keyword = keyword
         self.limit = limit
@@ -14,5 +14,5 @@ class Food:
         params = {'q': self.keyword,
                   'max': self.limit,
                   'api_key': self.key}
-        self.response = requests.get(base_url, params=params)
-        return self.response.text
+        response = requests.get(base_url, params=params).json()
+        return response
