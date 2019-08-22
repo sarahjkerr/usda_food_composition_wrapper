bd705b40bb3be75a1170a320fece1565dffb2285
Sarah K <sarah.j.kerr10@gmail.com>
2019-08-22 11:53:56 -0400

    moved url, added method, abstratced url building

--- food_query.py
+++ food_query.py
@@ -1,14 +1,23 @@
 import requests
 
-class Food:
+class FoodCompositionDBAdapter:
     
     def __init__(self, api_key):
         self.key = api_key
-        self.base_url = 'https://api.nal.usda.gov/ndb/search/?format=json'
+        self.base_url = 'https://api.nal.usda.gov/ndb/'
     
     def search_foods(self, keyword, limit):
-        params = {'q': keyword,
+        params = {'api_function': 'search',
+                  'format': 'json',
+                  'q': keyword,
                   'max': limit,
                   'api_key': self.key}
-        response = requests.get(self.base_url, params=params).json()
-        return response
+        return requests.get(self.base_url, params=params).json()
+    
+    def search_nutrients(self, food_group, limit):
+        params = {'api_function': 'nutrients',
+                  'format': 'json',
+                  'fg': food_group,
+                  'max': limit,
+                  'api_key': self.key}
+        return requests.get(self.base_url, params=params).json()
